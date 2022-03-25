1b3b62ec7a1754788d2f3a81020be1b439c3a482
Carter2307 <remirjunior1999@gmail.com>
2022-03-25 16:36:03 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+
+//EJS template engine
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+//Add static file  like js and css
+app.use(express.static(__dirname + "/public"));
+
+app.get('/', (req, res) => {
+  res.render('pages/home')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
